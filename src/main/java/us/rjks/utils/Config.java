391ec5e9d42a0b73800cbfb71550f61589a073bc
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-06-28 09:35:08 +0200

    added config

--- src/main/java/us/rjks/utils/Config.java
+++ src/main/java/us/rjks/utils/Config.java
@@ -5,6 +5,8 @@ import us.rjks.core.ProxyManager;
 import us.rjks.module.BungeeModule;
 import us.rjks.module.ModuleType;
 
+import java.util.ArrayList;
+
 /***************************************************************************
  *
  *  Urheberrechtshinweis
@@ -17,5 +19,60 @@ public class Config extends BungeeModule {
 
     public Config(Plugin plugin, String directory, String name, ModuleType type, boolean autoCreate) {
         super(plugin, directory, name, type, autoCreate);
+    }
+
+    public String getSting(String path) {
+        if (getCache().containsKey(path)) return getCache().get(path).toString();
+        Object ob = getConfig().get(path);
+        if (ob != null) {
+            getCache().put(path, ob);
+            return ob.toString();
+        } else {
+            return "";
+        }
+    }
+
+    public String getPermission(String path) {
+        if (getCache().containsKey("permission." + path)) return getCache().get("permission." + path).toString();
+        Object ob = getConfig().get("permission." + path);
+        if (ob != null) {
+            getCache().put("permission." + path, ob);
+            return ob.toString();
+        } else {
+            return "";
+        }
+    }
+
+    public Integer getInteger(String path) {
+        if (getCache().containsKey(path)) return Integer.getInteger(getCache().get(path).toString());
+        Object ob = getConfig().get(path);
+        if (ob != null) {
+            getCache().put(path, ob);
+            return Integer.getInteger(ob.toString());
+        } else {
+            return 0;
+        }
+    }
+
+    public Double getDouble(String path) {
+        if (getCache().containsKey(path)) return Double.parseDouble(getCache().get(path).toString());
+        Object ob = getConfig().get(path);
+        if (ob != null) {
+            getCache().put(path, ob);
+            return Double.parseDouble(ob.toString());
+        } else {
+            return 0.0;
+        }
+    }
+
+    public ArrayList<String> getStringList(String path) {
+        if (getCache().containsKey(path)) return (ArrayList<String>) getCache().get(path);
+        Object ob = getConfig().get(path);
+        if (ob != null) {
+            getCache().put(path, ob);
+            return (ArrayList<String>) getCache().get(path);
+        } else {
+            return new ArrayList<>();
+        }
     }
 }
